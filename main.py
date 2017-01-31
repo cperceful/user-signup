b1d3c8536603c9b16e08ba6e5b1e39f398c684fb
cperceful <cpercef1@gmail.com>
2017-01-30 19:51:52 -0600

    created success page, added error messages for invalid input

--- main.py
+++ main.py
@@ -14,19 +14,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
+import webapp2;
+import re;
+import cgi;
 
-def buildPage():
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$");
+def validUsername(username):
+    return USER_RE.match(username);
+
+PASSWORD_RE = re.compile(r"^.{3,20}$");
+def validPassword(password):
+    return PASSWORD_RE.match(password);
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$");
+def validEmail(email):
+    return EMAIL_RE.match(email);
+
+def buildPage(usernameError="", passwordError="", verifyError="", emailError=""):
     content = '''
         <!DOCTYPE html>
         <html>
             <head>
                 <title>User Signup</title>
-                <style media="screen">
-                    .error{
+                <style>
+                    .error{{
                         color: red;
                         font-weight: bold;
-                    }
+                    }}
                 </style>
             </head>
             <body>
@@ -35,32 +49,96 @@ def buildPage():
                     <table>
                         <tr>
                             <td><label for="username">Username</label></td>
-                            <td><input type="text" name="username" required><span class="error"></span></td>
+                            <td><input type="text" name="username" required><span class="error">{u}</span></td>
                         </tr>
                         <tr>
                             <td><label for="password">Password</label></td>
-                            <td><input type="password" name="password" required><span class="error"></span></td>
+                            <td><input type="password" name="password" required><span class="error">{p}</span></td>
                         </tr>
                         <tr>
                             <td><label for="verifypassword">Verify Password</label></td>
-                            <td><input type="password" name="verifypassword" required><span class="error"></span></td>
+                            <td><input type="password" name="verify" required><span class="error">{v}</span></td>
                         </tr>
                         <tr>
                             <td><label for="email">Email</label></td>
-                            <td><input type="email" name="email" value=""><span class="error"></span></td>
+                            <td><input type="text" name="email" value=""><span class="error">{e}</span></td>
                         </tr>
                     </table>
                         <input type="submit">
                 </form>
             </body>
         </html>
-    '''
+    '''.format(u=usernameError, p=passwordError, v=verifyError, e=emailError);
     return content;
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+
         self.response.write(buildPage());
 
+    def post(self):
+        username = self.request.get('username');
+        password = self.request.get('password');
+        verify = self.request.get('verify');
+        email = self.request.get('email');
+
+        if (validUsername(username) and validPassword(password) and validPassword(verify) and password == verify):
+            self.redirect('/success?username=' + username);
+
+        if not validUsername(username):
+            usernameError = 'Invalid username, idiot';
+        else:
+            usernameError = '';
+
+        if validPassword(password):
+            if password != verify:
+                verifyError = "Passwords do not match, idiot"
+                passwordError = "";
+            else:
+                verifyError = "";
+                passwordError = "Invalid password, idiot"
+        elif not validPassword(password):
+            verifyError = "";
+            passwordError = "Invalid password, idiot";
+
+        if email:
+            if not validEmail(email):
+                emailError = "Invalid email, idiot";
+            else:
+                emailError = "";
+        else:
+            emailError = "";
+
+        self.response.write(buildPage(usernameError=usernameError, passwordError=passwordError, verifyError=verifyError, emailError=emailError));
+
+
+
+
+        if email:
+            if not validEmail(email):
+                emailError = "Invalid emmail";
+            else:
+                emailError = "";
+        else:
+            emailError = "";
+
+
+
+
+
+
+
+class SuccessHandler(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username');
+
+        content = '''
+        <h1>Thanks for signing up, {u}! You're the best and I love you.</h1>
+        '''.format(u=username);
+
+        self.response.write(content);
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/success', SuccessHandler)
 ], debug=True)
